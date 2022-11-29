9a208d0177ca61f6e951e9336350c176bbb6a816
Dean Reymen <reymen@outlook.be>
2022-11-29 15:38:13 +0400

    rollback

--- src/app/effort-form/effort-form.component.ts
+++ src/app/effort-form/effort-form.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 import { EffortService } from "../effort.service";
 
 import { Effort } from "../effort";
+import { connect } from 'rxjs';
+import { identifierName } from '@angular/compiler';
 
 @Component({
   selector: 'app-effort-form',
@@ -12,87 +15,58 @@ import { Effort } from "../effort";
   styleUrls: ['./effort-form.component.scss']
 })
 export class EffortFormComponent implements OnInit {
-  public effortForm!: FormGroup; 
+  public effortForm: FormGroup = new FormGroup({
+    description: new FormControl('', [Validators.required]),
+    URL: new FormControl(''),
+    time: new FormControl(''),
+    category: new FormControl('', [Validators.required])
+  });
+
   public submitted: boolean = false;
-  public effort?: Effort;
-
-  public constructor(
+  public effort?: Effort; 
+  
+  constructor(
     private route: ActivatedRoute,
-    private router: Router,
-    private effortService: EffortService,
-    private formBuilder: FormBuilder) { }
+    private effortService: EffortService, 
+    private location: Location) { }
 
-  public ngOnInit(): void {
-    
+  ngOnInit(): void {
     this.getEffort();
   }
 
-  public goToEffortRoute(): void {
-    this.router.navigate(['/effort'], { relativeTo: this.route });
+  goBack(): void {
+    this.location.back();
   }
 
   public onSubmit(): void {
     this.submitted = true;
-    if (this.effortForm.valid) {
-      if (this.effort) {
+    if (this.effort) {
+      if (this.effortForm.valid) {
         this.effort.description = this.effortForm.get('description')?.value;
         this.effort.URL = this.effortForm.get('URL')?.value;
         this.effort.time = this.effortForm.get('time')?.value;
         this.effort.category = this.effortForm.get('category')?.value;
-  
+
         this.effortService.updateEffort(this.effort)
-          .subscribe(_ => {
-            console.log(_);
-            this.goToEffortRoute();
-          });
+          .subscribe(() => this.goBack());
       }
-      else {
-        const createdAt: string = new Date().toISOString();
-        let effort: Effort = new Effort(
-          1,
-          this.effortForm.get('description')?.value,
-          this.effortForm.get('category')?.value,
-          createdAt,
-          this.effortForm.get('URL')?.value,
-          this.effortForm.get('time')?.value,
-        );
+    } else {
+      const createdAt: Date = new Date();
+      if (this.effortForm.valid) {
+        let effort: Effort = {...this.effortForm.value, createdAt};
         this.effortService.addEffort(effort)
-          .subscribe(_ => {
-            console.log(_);
-            this.goToEffortRoute();
-          });
+          .subscribe(() => this.goBack());
       }
     }
   }
 
-  public getEffort(): void {
-    this.initialiseForm();
-
+  getEffort(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    
-    console.log(id);
-    if (id !== 0) {
-    
-      this.effortService.getEffort(id)
-        .subscribe(effort => {
-          console.log(effort);
-          this.effort = effort;
-          this.effortForm.setValue({
-            description: effort.description, 
-            URL: effort.URL,
-            time: effort.time,
-            category: effort.category
-          });
-        });
-    }
+    this.effortService.getEffort(id)
+      .subscribe(effort => {
+        this.effort = effort;
+        this.effortForm.patchValue({...this.effort});
+      });
   }
 
-  private initialiseForm(): void {
-    this.effortForm = this.formBuilder.group({
-      description: [null, [Validators.required]],
-      URL: [null, Validators.pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)],
-      time: null,
-      category: [null, [Validators.required]]
-    });
-  }
 }
